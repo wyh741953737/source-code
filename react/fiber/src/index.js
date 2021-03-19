d7da59c30653b72066f81a7a4d2493b3a9a3e950
wyh741953737 <741953737@qq.com>
2021-03-19 23:37:44 +0800

    redu-saga

--- react/fiber/src/index.js
+++ react/fiber/src/index.js
@@ -1,4 +1,4 @@
-import React from './react';
+import React from 'react';
 import ReactDom from './react-dom';
 
 const element = (
@@ -11,6 +11,18 @@ const element = (
     </div>
 )
 
-console.log(element)
+function Fn(props) {
+    return (
+        <div id="A1" style={{border: '1px solid  red'}}>
+            <div id="B1" style={{border: '1px solid green'}}>
+                    <div id="C1" style={{fontSize: '24px'}}>C1</div>
+                    <div id="C2" style={{fontSize: '24px'}}>C2</div>
+            </div>
+            <div id="B2">B2</div>
+        </div>
+    )
+}
+
+
 
 ReactDom.render(element,  document.getElementById('root'));
---
+++ redux/redux-sage/readme.md
@@ -0,0 +1,3 @@
+rootSage根，入口
+workSaga： 工作者
+watcherSaga： 监听者
