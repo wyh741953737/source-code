f6bcf065747a1cef80f4697027762f70dda0385b
wyh741953737 <741953737@qq.com>
2021-05-21 13:38:45 +0800

    save

---
+++ 异步/index.js
@@ -0,0 +1,10 @@
+setTimeout(() => {
+  console.log('a')   
+});
+Promise.resolve().then(() => console.log('b')).then(() => Promise.resolve('c').then((data) => {
+    setTimeout(() => {
+      console.log('d')   
+    });
+    console.log('f')
+    return data;
+})).then(d => console.log(d))
